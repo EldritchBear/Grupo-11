9fc3c03822db3ef0a015f2199990820bdd37f83f
Lekkice <lekkice0@gmail.com>
2022-10-24 00:02:59 -0300

    agregada clase abstracta Nivel

---
+++ core/src/com/mygdx/game/Nivel.java
@@ -0,0 +1,22 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+
+public abstract class Nivel {
+    Nave nave;
+    private int vidas = 3;
+    Nivel () {}
+
+    public void generarNivel() {
+        nave = new Nave(Gdx.graphics.getWidth()/2-50,30,new Texture(Gdx.files.internal("MainShip3.png")),
+                Gdx.audio.newSound(Gdx.files.internal("hurt.ogg")),
+                new Texture(Gdx.files.internal("Rocket2.png")),
+                Gdx.audio.newSound(Gdx.files.internal("pop-sound.mp3")));
+        nave.setVidas(vidas);
+
+        generarAsteroides();
+    }
+
+    abstract Asteroides generarAsteroides();
+}
