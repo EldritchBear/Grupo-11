a9fab6d5a1b21c2f566c5ef260f51b1d91ba1eb3
Lekkice <lekkice0@gmail.com>
2022-10-25 21:58:16 -0300

    arreglos a clase NivelNormal

--- core/src/com/mygdx/game/NivelNormal.java
+++ core/src/com/mygdx/game/NivelNormal.java
@@ -5,17 +5,20 @@ import com.badlogic.gdx.graphics.Texture;
 
 import java.util.Random;
 
-public class NivelNormal implements Nivel{
-    public Asteroides generarAsteroides(int cantAsteroides, int velXAsteroides, int velYAsteroides){
+public class NivelNormal extends Nivel {
+    public Asteroides generarAsteroides(){
+        int cantAsteroides = 5;
+        int velXAsteroides = 1;
+        int velYAsteroides = 1;
         Random r = new Random();
-        Asteroides asteroidesNivel = new Asteroides();
-        for (int i = 0; i < cantAsteroides; i++) {
-            Asteroides bb = new Asteroides(r.nextInt(Gdx.graphics.getWidth()),
-                    50+r.nextInt(Gdx.graphics.getHeight()-50),
-                    20+r.nextInt(10), velXAsteroides+r.nextInt(4), velYAsteroides+r.nextInt(4),
-                    new Texture(Gdx.files.internal("aGreyMedium4.png")),
-                    2);
-            asteroidesNivel.agregarAsteroides(bb);
+        Asteroides asteroides = new Asteroides();
+        for (int i = 0; i < cantAsteroides; i++) { // usar subclase de Asteroide
+//            Asteroide bb = new Asteroide(r.nextInt(Gdx.graphics.getWidth()),
+//                    50+r.nextInt(Gdx.graphics.getHeight()-50),
+//                    20+r.nextInt(10), velXAsteroides+r.nextInt(4), velYAsteroides+r.nextInt(4),
+//                    new Texture(Gdx.files.internal("aGreyMedium4.png")),
+//                    2);
         }
+        return asteroides;
     }
 }
